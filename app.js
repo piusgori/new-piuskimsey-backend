ece67b4c1fd172c7b394474d9a3ef7e017d06ebd
piusgori <piusgori@gmail.com>
2022-08-23 13:51:56 +0300

    made small changes

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const http = require('http');
-const { Server } = require('socket.io');
 
 const HttpError = require('./models/http-error');
 const mainRoute = require('./routes/main-route');
@@ -11,13 +9,6 @@ const authRoute = require('./routes/auth-route');
 // const privateKeys = require('./private-keys');
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST', 'PATCH', 'DELETE']
-    }
-});
 
 const mongoUrl = `mongodb+srv://pius_gori:${process.env.mongoPassword}@piuscluster.wvoqx.mongodb.net/piuskimsey?retryWrites=true&w=majority`
 
@@ -43,14 +34,8 @@ app.use((error, req, res, next) => {
     res.json({ message: error.message || 'An Unknown error has occurred!', content: error.content || null })
 })
 
-io.on('connection', (socket) => {
-    socket.on("send_message", (data) => {
-        socket.broadcast.emit("receive_message", data);
-    })
-})
-
 mongoose.connect(mongoUrl).then(() => {
-    server.listen(process.env.PORT || 8000);
+    app.listen(process.env.PORT || 8000);
     console.log('Server started');
 }
 ).catch(err => {
